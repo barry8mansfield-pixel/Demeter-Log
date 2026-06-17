a9a52f437b03817f3e71eb65bcfb57cbef491fd9
barry8mansfield-pixel <barry8mansfield@gmail.com>
2026-06-17 15:34:10 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE = '1778041687';
-const ASSETS = ['/', '/index.html', '/growers.js', '/manifest.json', '/icon-192.png', '/icon-512.png', '/apple-touch-icon.png'];
-self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); });
-self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
-self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).then(res => { const clone = res.clone(); caches.open(CACHE).then(c => c.put(e.request, clone)); return res; }).catch(() => caches.match(e.request))); });
